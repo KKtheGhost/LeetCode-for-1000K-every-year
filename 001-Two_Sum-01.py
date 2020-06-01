d71de12916aad2bb86914f52607f328c28e025f4
Kivinsae Fang <32515871+KKtheGhost@users.noreply.github.com>
2020-06-01 16:03:22 +0800

    Update 001-Two_Sum-01.py

--- 001-Two_Sum-01.py
+++ 001-Two_Sum-01.py
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
-## Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-## 
-## You may assume that each input would have exactly one solution, and you may not use the same element twice.
-## 
-## Example:
-## 
-## Given nums = [2, 7, 11, 15], target = 9,
-## 
-## Because nums[0] + nums[1] = 2 + 7 = 9,
-## return [0, 1].
-
-=======
->>>>>>> 0229a893e36caee8674fa2dba214e502d5a758fe
 class Solution(object):
     def twoSum(self, nums, target):
-        keys = {}
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
         for i in range(len(nums)):
-            if target - nums[i] in keys:
-                return [keys[target - nums[i]], i]
-            if nums[i] not in keys:
-<<<<<<< HEAD
-                keys[nums[i]] = i     
-=======
-                keys[nums[i]] = i      
->>>>>>> 0229a893e36caee8674fa2dba214e502d5a758fe
+            if (target - nums[i]) in dic:
+                return [i,dic[target-nums[i]]]
+            dic[nums[i]] = i 
